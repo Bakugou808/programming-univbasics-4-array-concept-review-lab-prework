a6cc8bc7062c46baa8f0ac8797214dc194cb3186
Janu Sung <janu.m.sung@gmail.com>
2020-02-14 00:52:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,8 +11,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  max = 0
   array.length.times do |idx|
-    max = 0 
     
     if max < array[idx]
       max = array[idx]
